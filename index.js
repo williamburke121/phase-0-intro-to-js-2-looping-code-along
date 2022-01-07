3c6713bb9ac543e64084861f285b627f4ddadcc3
William Warner Burke <williamburke121@gmail.com>
2022-01-06 22:19:17 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki'];
+const event = 'suprise'
+
+function writeCards(name, event) {
+    let newArray = [];
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+
+    return newArray;
+}
+
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
